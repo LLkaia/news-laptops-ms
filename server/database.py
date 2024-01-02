d122331fb2bdb2d6c05712fe0486368e5ed49e65
Illia Kaialainien <illia.kaialainien@gmail.com>
2024-01-02 16:32:35 +0200

    added Dockerfile

--- server/database.py
+++ server/database.py
@@ -8,7 +8,7 @@ from server.scraper import scrap_from_search
 from server.models.search_result import Period
 
 
-MONGO_DETAILS = 'mongodb://localhost:27017'
+MONGO_DETAILS = 'mongodb://mongodb:27017'
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 db = client.news
 
