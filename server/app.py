2c5b0af8d241591d5cd24f9836db5fe1f174d539
Illia Kaialainien <illia.kaialainien@gmail.com>
2024-01-11 18:54:12 +0200

    added cors

--- server/app.py
+++ server/app.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from server.routes.search_result import router as SearchResultRouter
 
 
 app = FastAPI()
 app.include_router(SearchResultRouter, tags=["News"], prefix="/news")
+
+
+origins = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
